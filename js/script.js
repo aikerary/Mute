4bcff7b9ae4eecc71779b995a8587e2da5caff99
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-04-26 09:56:11 -0500

    Adding a test

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Create a window onload function
+window.onload = function() {
+    // Create a variable to store the element with the id of "btn"
+    var btn = document.getElementById("btn");
+};
