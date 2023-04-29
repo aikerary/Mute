1fd1a12c9637ceac2ddc814503b70cbc0ebcee0a
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-04-29 16:14:01 -0500

    Fixing some wheel bugs while building it

--- js/wheel.js
+++ js/wheel.js
@@ -4,16 +4,16 @@ window.onload = function() {
     var toggles = document.getElementsByClassName('toggle');
     // Select all the items with the class name menu
     var menus = document.getElementsByClassName('menu');
-    // Use an event listener for toggle to toggle the class name active
-    for (var i = 0; i < toggles.length; i++) {
-        toggles[i].addEventListener('click', function() {
-            this.classList.toggle('active');
-        });
-    }
-    // Use an event listener for menu to toggle the class name active
-    for (var i = 0; i < menus.length; i++) {
-        menus[i].addEventListener('click', function() {
-            this.classList.toggle('active');
-        });
-    }
+    // With toggle 0, add an event listener for a click
+    toggles[0].addEventListener('click', function() {
+        // Toogle the class "active" for the menu
+        menus[0].classList.toggle('active');
+        toggles[0].classList.toggle('active');
+    });
+    // With toggle 1, add an event listener for a click
+    toggles[1].addEventListener('click', function() {
+        // Toogle the class "active" for the menu
+        menus[1].classList.toggle('active');
+        toggles[1].classList.toggle('active');
+    });
 };
