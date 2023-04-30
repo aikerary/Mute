7fb71f64e93c974eb8abffc199e2fd1f795ef1b3
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-04-30 17:26:57 -0500

    Fixing the other elements to change when the bulblight is active

--- js/bulb.js
+++ js/bulb.js
@@ -5,5 +5,7 @@ window.addEventListener('load', function() {
     // When clicked, toggle the class active
     mouseketool.addEventListener('click', function() {
         mouseketool.classList.toggle('active');
+        // Toggle the class "bulbed" on the body
+        document.body.classList.toggle('bulbed');
     });
 });
