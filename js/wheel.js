4b8a843ae2a2d7094440a32ec4a1c0b361983473
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-04-29 23:50:57 -0500

    Adding the border function

--- js/wheel.js
+++ js/wheel.js
@@ -16,4 +16,52 @@ window.onload = function() {
         menus[1].classList.toggle('active');
         toggles[1].classList.toggle('active');
     });
+    // Get all the childs of menu 0 that are li
+    var menuItems0 = menus[0].getElementsByTagName('li');
+    function activeLi0(){
+        // For each item in the menu
+        for (var i = 0; i < menuItems0.length; i++) {
+            // If the item is clicked
+            menuItems0[i].addEventListener('click', function() {
+                // Remove the class active from all the items
+                for (var j = 0; j < menuItems0.length; j++) {
+                    menuItems0[j].classList.remove('active');
+                }
+                // If the item doesn't have the class active
+                // Add the class active to the clicked item
+                // Else remove the class active from the clicked item
+                if (!this.classList.contains('active')) {
+                    this.classList.add('active');
+                }
+                else {
+                    this.classList.remove('active');
+                }
+            });
+        }
+    }
+    activeLi0();
+    // Get all the childs of menu 1 that are li
+    var menuItems1 = menus[1].getElementsByTagName('li');
+    function activeLi1(){
+        // For each item in the menu
+        for (var i = 0; i < menuItems1.length; i++) {
+            // If the item is clicked
+            menuItems1[i].addEventListener('click', function() {
+                // Remove the class active from all the items
+                for (var j = 0; j < menuItems1.length; j++) {
+                    menuItems1[j].classList.remove('active');
+                }
+                // If the item doesn't have the class active
+                // Add the class active to the clicked item
+                // Else remove the class active from the clicked item
+                if (!this.classList.contains('active')) {
+                    this.classList.add('active');
+                }
+                else {
+                    this.classList.remove('active');
+                }
+            });
+        }
+    }
+    activeLi1();
 };
