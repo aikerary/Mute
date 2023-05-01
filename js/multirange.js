3f65f4c1cf6f70738c826ddd3d8d2baed344c31f
Aiker Ary Acosta Cantillo <aikiringo26@gmail.com>
2023-04-30 20:59:34 -0500

    Adding the multi-slider

---
+++ js/multirange.js
@@ -0,0 +1,36 @@
+// Create an event listener to load window
+window.addEventListener('load', function() {
+    var lowerSlider = document.querySelector('#lower'),
+   upperSlider = document.querySelector('#upper'),
+   lowerVal = parseInt(lowerSlider.value);
+   upperVal = parseInt(upperSlider.value);
+
+upperSlider.oninput = function() {
+   lowerVal = parseInt(lowerSlider.value);
+   upperVal = parseInt(upperSlider.value);
+   
+   if (upperVal < lowerVal + 4) {
+      lowerSlider.value = upperVal - 4;
+      
+      if (lowerVal == lowerSlider.min) {
+         upperSlider.value = 4;
+      }
+   }
+};
+
+
+lowerSlider.oninput = function() {
+   lowerVal = parseInt(lowerSlider.value);
+   upperVal = parseInt(upperSlider.value);
+   
+   if (lowerVal > upperVal - 4) {
+      upperSlider.value = lowerVal + 4;
+      
+      if (upperVal == upperSlider.max) {
+         lowerSlider.value = parseInt(upperSlider.max) - 4;
+      }
+
+   }
+};
+
+});
